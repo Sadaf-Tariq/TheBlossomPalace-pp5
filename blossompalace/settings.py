66c85ae6ff4eedbe06f8b2a9f1e88b744346b999
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2024-02-27 17:43:22 +0000

    update allowed hosts

--- blossompalace/settings.py
+++ blossompalace/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sadaftariq-theblossompa-quamqy6usa6.ws-eu108.gitpod.io',]
+ALLOWED_HOSTS = ['8000-sadaftariq-theblossompa-quamqy6usa6.ws-eu108.gitpod.io','theblossompalace-a9bac6e32f8a.herokuapp.com']
 
 
 # Application definition
