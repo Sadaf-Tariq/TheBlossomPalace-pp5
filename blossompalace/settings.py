9b50627eedb112534292d0064337eb748cbf22f3
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2024-02-29 22:19:25 +0000

    Add crispy forms

--- blossompalace/settings.py
+++ blossompalace/settings.py
@@ -54,6 +54,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -69,6 +72,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blossompalace.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -86,6 +91,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
