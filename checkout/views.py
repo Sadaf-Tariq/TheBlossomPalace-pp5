73056fc0f7cf2f881314e776b44ff3ff0e62314c
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2024-03-01 11:11:18 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OpTl3B5BjdUEFZ1Y6Whi1A2JjvxKP8fC1r2z5G9myc2FkqhDk1rWDp6BJ5S7tTtnrll1aN35OO8gGsU6lrXVLF100t3YkK8hS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
